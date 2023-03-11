edcfbe59b1f947c5b748efea84b81c380377264f
Edelmetall <chime.switzerland@gmail.com>
2023-03-11 13:13:03 +0100

    init

--- src/main/java/ch/parkship/springazuredemoedelmetall/SpringAzureDemoEdelmetallApplication.java
+++ src/main/java/ch/parkship/springazuredemoedelmetall/SpringAzureDemoEdelmetallApplication.java
@@ -2,11 +2,17 @@ package ch.parkship.springazuredemoedelmetall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
-public class SpringAzureDemoEdelmetallApplication{
+public class SpringAzureDemoEdelmetallApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringAzureDemoEdelmetallApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAzureDemoEdelmetallApplication.class, args);
